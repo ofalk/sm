902abdec31ca4ef6e89a152837801cb2903534ec
Oliver Falk <oliver@linux-kernel.at>
2017-07-31 10:57:22 +0200

    Server model tests

---
+++ sm/server/test_server_model.py
@@ -0,0 +1,46 @@
+import unittest
+
+import os
+import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sm.settings'
+django.setup()
+
+
+class ServerTestCase(unittest.TestCase):
+    def test_0_server_import(self):
+        from server.models import Server as ServerModel
+        self.assertEqual(ServerModel, ServerModel, 'import went wrong')
+
+    def test_1_server_creation(self):
+        from server.models import Server as ServerModel
+        server, created = ServerModel.objects.get_or_create(
+            hostname='virtualXXX123'
+        )
+        self.assertEqual(created, True, 'the object was already there?')
+        self.assertIsInstance(server, ServerModel,
+                              'object not a Location model!?')
+
+    def test_2_server_hostname(self):
+        from server.models import Server as ServerModel
+        server = ServerModel.objects.get(hostname='virtualXXX123')
+        self.assertEqual(server.hostname, 'virtualXXX123', 'hostname not correct')
+
+    def test_3_server_hostname__str__(self):
+        from server.models import Server as ServerModel
+        server = ServerModel.objects.get(hostname='virtualXXX123')
+        self.assertEqual("%s" % server, 'virtualXXX123', 'hostname not correct')
+
+    def tearDownClass():
+        """
+        Make sure we delete our test object at the end
+        """
+        from server.models import Server as ServerModel
+        try:
+            server = ServerModel.objects.get(hostname='virtualXXX123')
+            server.delete()
+        except Exception as e:
+            pass
+
+
+if __name__ == '__main__':
+    unittest.main()
