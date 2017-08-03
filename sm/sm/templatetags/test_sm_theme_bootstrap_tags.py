4961b5230d530ad75504ffa3de8a28796c2da5f3
Oliver Falk <oliver@linux-kernel.at>
2017-08-03 08:15:58 +0200

    Add tests for get_message_tags

---
+++ sm/sm/templatetags/test_sm_theme_bootstrap_tags.py
@@ -0,0 +1,69 @@
+from django.test import TestCase
+from django.contrib.messages import constants as DEFAULT_MESSAGE_LEVELS
+from django.template import Context, Template
+
+import os
+import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sm.settings'
+django.setup()
+
+
+class TestCase(TestCase):
+    class FakeMessage(object):
+        """
+        Follows the `django.contrib.messages` API
+        """
+        level = None
+        message = None
+        extra_tags = None
+
+        def __init__(self, level, message, extra_tags=None):
+            self.level = level
+            self.extra_tags = extra_tags
+            self.message = message
+
+        def __str__(self):
+            return self.message
+
+    def test_00_load(self):
+        string = '{% load sm_theme_bootstrap_tags %}'
+        rendered = Template(string).render(Context({}))
+        self.assertEqual(rendered, '')
+
+    def test_01_warning(self):
+        string = '{% load sm_theme_bootstrap_tags %}'
+        string += '{% get_message_tags message %}'
+        rendered = Template(string).render(Context({
+            'message': TestCase.FakeMessage(DEFAULT_MESSAGE_LEVELS.WARNING,
+                                            'warning message')
+        }))
+        self.assertEqual(rendered, 'alert-warning')
+
+    def test_02_error(self):
+        string = '{% load sm_theme_bootstrap_tags %}'
+        string += '{% get_message_tags message %}'
+        rendered = Template(string).render(Context({
+            'message': TestCase.FakeMessage(DEFAULT_MESSAGE_LEVELS.ERROR,
+                                            'error message')
+        }))
+        # This test is very special, since it tests if error is correctly
+        # translated into danger alert tag (Bootstrap 3)
+        self.assertEqual(rendered, 'alert-danger')
+
+    def test_02_warning_with_extra_tag(self):
+        string = '{% load sm_theme_bootstrap_tags %}'
+        string += '{% get_message_tags message %}'
+        rendered = Template(string).render(Context({
+            'message': TestCase.FakeMessage(DEFAULT_MESSAGE_LEVELS.WARNING,
+                                            'message', 'testtag')
+        }))
+        self.assertEqual(rendered, 'testtag alert-warning')
+
+    def test_03_extra_tag_only(self):
+        string = '{% load sm_theme_bootstrap_tags %}'
+        string += '{% get_message_tags message %}'
+        rendered = Template(string).render(Context({
+            'message': TestCase.FakeMessage(None,
+                                            'message', 'testtag')
+        }))
+        self.assertEqual(rendered, 'testtag')
