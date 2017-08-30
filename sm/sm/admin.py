6588a7d7b0fff7b753dfad433485a108851ab533
Oliver Falk <oliver@linux-kernel.at>
2017-08-30 12:54:25 +0200

    Add Domain model

--- sm/sm/admin.py
+++ sm/sm/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from patchtime.models import Patchtime
 from server.models import Server
 from status.models import Status
+from domain.models import Domain
 from location.models import Location
 from vendor.models import Vendor
 from operatingsystem.models import Operatingsystem
@@ -10,6 +11,7 @@ from operatingsystem.models import Operatingsystem
 admin.site.register(Patchtime)
 admin.site.register(Server)
 admin.site.register(Status)
+admin.site.register(Domain)
 admin.site.register(Location)
 admin.site.register(Vendor)
 admin.site.register(Operatingsystem)
