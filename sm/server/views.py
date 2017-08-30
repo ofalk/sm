5af293570da276c700c0d3ead9a3c7fb76693371
Oliver Falk <oliver@linux-kernel.at>
2017-08-30 12:53:59 +0200

    Initial test of generic update view with boostrap

--- sm/server/views.py
+++ sm/server/views.py
@@ -5,11 +5,31 @@ from account.mixins import LoginRequiredMixin
 
 from . models import Server
 
+from django import forms
 
-class ServerView(LoginRequiredMixin, ListView):
+from django.views.generic.edit import UpdateView
+
+
+class ServerListView(LoginRequiredMixin, ListView):
     template_name = 'server/list.html'
     model = Server
     paginate_by = 20
     queryset = model.objects.all()
     orphans = 3
-    ordering = '-id'
+    ordering = 'hostname'
+
+
+class ServerForm(forms.ModelForm):
+    class Meta:
+        model = Server
+        fields = '__all__'
+        widgets = {
+            'delivery_date': forms.DateInput(attrs={'class': 'date-input'}),
+            'install_date': forms.DateInput(attrs={'class': 'date-input'})
+        }
+
+
+class ServerDetailView(UpdateView):
+    template_name = 'server/detail.html'
+    form_class = ServerForm
+    model = Server
