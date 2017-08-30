c64a53c2e02ad8801637e3817af0443ecf5f6204
Oliver Falk <oliver@linux-kernel.at>
2017-08-30 12:52:41 +0200

    Fix date fields and add get_absolute_url class function

--- sm/server/models.py
+++ sm/server/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from patchtime.models import Patchtime
 from status.models import Status
+from domain.models import Domain
+from location.models import Location
+from django.urls import reverse
+from django.utils.timezone import now
 
 
 class Server(models.Model):
     hostname = models.CharField(max_length=45)
-    delivery_date = models.DateTimeField(auto_now=True)
-    install_date = models.DateTimeField(auto_now=True)
-    documentation_url = models.URLField(max_length=2083)
+    domain = models.ForeignKey(Domain, on_delete=models.PROTECT, default=1)
+    delivery_date = models.DateField(default=now)
+    install_date = models.DateField(default=now)
+    last_update = models.DateTimeField(auto_now=True)
+    documentation_url = models.URLField(max_length=2083, blank=True, null=True)
     memory_in_mb = models.IntegerField(blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.PROTECT)
 
     status = models.ForeignKey(Status, on_delete=models.PROTECT, default=1)
 
@@ -34,6 +41,9 @@ class Server(models.Model):
 
     def __str__(self):
         return "%s" % self.hostname
+
+    def get_absolute_url(self):
+        return reverse('server:detail', kwargs={'pk': self.pk})
 
     class Meta:
         managed = True
