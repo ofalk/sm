bd23103a6edb6794e0a07e27f3e6376cd3a8708c
Oliver Falk <oliver@linux-kernel.at>
2017-09-05 14:02:04 +0200

    Test cleanup

--- sm/location/test_location_model.py
+++ sm/location/test_location_model.py
@@ -1,4 +1,4 @@
-import unittest
+from django.test import TransactionTestCase as TestCase
 
 from location.models import Location as Model
 
@@ -8,47 +8,54 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'sm.settings'
 django.setup()
 
 
-class Tester(unittest.TestCase):
+class Tester(TestCase):
     model = Model
-    name = 'Virtual123XXX'
+    teststring = 'Virtual123XXX'
 
-    def test_1_creation(self):
-        item, created = self.model.objects.get_or_create(name=self.name)
+    @classmethod
+    def createTestItem(self):
+        item, created = self.model.objects.get_or_create(
+           name=self.teststring
+        )
+        return (item, created)
+
+    def test_01_creation(self):
+        item, created = self.createTestItem()
         self.assertEqual(created, True, 'the object was already there?')
         self.assertIsInstance(item, self.model, 'object not a Location model!')
 
-    def test_2_name(self):
-        item = self.model.objects.get(name=self.name)
-        self.assertEqual(item.name, self.name, 'name not correct')
+    def test_02_name(self):
+        item, created = self.createTestItem()
+        self.assertEqual(item.name, self.teststring, 'name not correct')
 
-    def test_3_name___str___wo_country(self):
-        item = self.model.objects.get(name=self.name)
-        self.assertEqual("%s" % item.name, self.name, 'name not correct')
+    def test_03_name___str___wo_country(self):
+        item, created = self.createTestItem()
+        self.assertEqual("%s" % item.name, self.teststring, 'name not correct')
 
-    def test_4___str___w_country(self):
-        item = self.model.objects.get(name=self.name)
+    def test_04___str___w_country(self):
+        item, created = self.createTestItem()
         item.country = 'Austria'
-        self.assertEqual("%s" % item, '%s / Austria' % self.name,
+        self.assertEqual("%s" % item, '%s / Austria' % self.teststring,
                          'name not correct')
 
-    def test_4___str___w_nonexistant_country(self):
-        item = self.model.objects.get(name=self.name)
+    def test_04___str___w_nonexistant_country(self):
+        item, created = self.createTestItem()
         item.country = 'Mars'
-        self.assertEqual("%s" % item, '%s / Mars' % self.name,
+        self.assertEqual("%s" % item, '%s / Mars' % self.teststring,
                          'name not correct')
 
-    def test_5_nonexisting_country(self):
-        item = self.model.objects.get(name=self.name)
+    def test_05_nonexisting_country(self):
+        item, created = self.createTestItem()
         item.country = 'Mars'
         self.assertEqual(item.country.flag_url, None)
 
-    def test_6___str__wo_country(self):
-        item = self.model.objects.get(name=self.name)
+    def test_06___str__wo_country(self):
+        item, created = self.createTestItem()
         item.country = None
-        self.assertEqual(item.__str__(), self.name)
+        self.assertEqual(item.__str__(), self.teststring)
 
-    def test_7_delete(self):
-        item = self.model.objects.get(name=self.name)
+    def test_07_delete(self):
+        item, created = self.createTestItem()
         res = item.delete()
         self.assertEqual(res[0], 1)
         self.assertTrue('sm.Location' in res[1])
