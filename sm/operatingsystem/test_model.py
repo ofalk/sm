189c16cf066afe3e420075a0fef6f8b309528d1c
Oliver Falk <oliver@linux-kernel.at>
2017-09-05 12:07:22 +0200

    Add fixtures for testing, use TransactionTestCase from Django

--- sm/operatingsystem/test_model.py
+++ sm/operatingsystem/test_model.py
@@ -1,4 +1,4 @@
-import unittest
+from django.test import TransactionTestCase as TestCase
 
 from operatingsystem.models import Operatingsystem as Model
 from vendor.models import Vendor as VendorModel
@@ -9,36 +9,43 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'sm.settings'
 django.setup()
 
 
-class Tester(unittest.TestCase):
+class Tester(TestCase):
     model = Model
     vendor = None
     teststring = '123XXX'
+    fixtures = ['fixtures/02_vendor.yaml', 'fixtures/05_operatingsystem.yaml']
 
+    @classmethod
     def setUp(self):
         self.vendor, created = VendorModel.objects.get_or_create(
             name='Red Hat')
 
-    def test_01_creation(self):
+    @classmethod
+    def createTestItem(self):
         item, created = self.model.objects.get_or_create(
             vendor=self.vendor,
             version=self.teststring
         )
+        return (item, created)
+
+    def test_01_creation(self):
+        (item, created) = self.createTestItem()
         self.assertEqual(created, True, 'the object was already there?')
         self.assertIsInstance(item, self.model,
                               'object not a Operatingsystem model!?')
 
     def test_02_version(self):
-        item = self.model.objects.get(version=self.teststring)
+        (item, created) = self.createTestItem()
         self.assertEqual(item.version, self.teststring, 'name not correct')
 
     def test_03__str__(self):
-        item = self.model.objects.get(version=self.teststring)
+        (item, created) = self.createTestItem()
         self.assertEqual("%s" % item, '%s %s' %
                          (self.vendor.name, self.teststring),
                          'name not correct')
 
     def test_04__natural_key__(self):
-        item = self.model.objects.get(version=self.teststring)
+        (item, created) = self.createTestItem()
         self.assertEqual(item.natural_key(),
                          (self.vendor.name, self.teststring))
 
@@ -71,52 +78,50 @@ class Tester(unittest.TestCase):
         self.assertIsInstance(item, self.model,
                               'object not a Operatingsystem model!?')
 
-    def test_10_get_or_create_sles_10(self):
+    def test_10_get_or_create_sles_10_plus_nat_key(self):
         from vendor.models import Vendor as VendorModel
         vendor, created = VendorModel.objects.get_or_create(name='Novell')
         item, created = self.model.objects.get_or_create(vendor=vendor,
                                                          version='10.0')
         self.assertIsInstance(item, self.model,
                               'object not a Operatingsystem model!?')
-
-    def test_11_get_sles_10_by_nat_key(self):
         item = self.model.objects.get_by_natural_key('SLES 10.0')
         self.assertIsInstance(item, self.model,
                               'object not a Operatingsystem model!?')
 
-    def test_12_nat_key_funny_doesnt_exist(self):
+    def test_11_nat_key_funny_doesnt_exist(self):
         from django.core.exceptions import ObjectDoesNotExist
         with self.assertRaises(ObjectDoesNotExist) as context:
             self.model.objects.get_by_natural_key('Hugo Boss 7.0')
         self.assertTrue('matching query does not exist' in
                         str(context.exception))
 
-    def test_13_nat_key_vendor_version_exists(self):
+    def test_12_nat_key_vendor_version_exists(self):
         item = self.model.objects.get_by_natural_key(
             vendor=self.vendor.name,
             version='7.0')
         self.assertIsInstance(item, self.model,
                               'object not a Operatingsystem model!?')
 
-    def test_14_natkey_get_with_tuple(self):
+    def test_13_natkey_get_with_tuple(self):
         item = self.model.objects.get_by_natural_key(
             ('%s' % self.vendor.name, '7.0'))
         self.assertIsInstance(item, self.model,
                               'object not a Operatingsystem model!?')
 
-    def test_15_query_with_dict_exception(self):
+    def test_14_query_with_dict_exception(self):
         with self.assertRaises(Exception) as context:
             self.model.objects.get_by_natural_key({})
         self.assertTrue("No idea how to handle query with <class 'dict'>" in
                         str(context.exception))
 
-    def test_16_get_absolute_url(self):
-        item = self.model.objects.get(version=self.teststring)
+    def test_15_get_absolute_url(self):
+        (item, created) = self.createTestItem()
         self.assertEqual('/operatingsystem/detail/%i/' % item.id,
                          item.get_absolute_url(),
                          'reverse url not correct')
 
-    def test_17_get_initial(self):
+    def test_16_get_initial(self):
         from operatingsystem.views import OperatingsystemCreateView as View
         v = View()
         v.kwargs = {'vendor': self.vendor.name}
