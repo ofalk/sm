2a1d82b4dd71c0adfc9f129c72397a1f25ca752a
Oliver Falk <oliver@linux-kernel.at>
2017-09-07 13:09:28 +0200

    Need to rename it in the initial migration as well

--- sm/status/migrations/0001_initial.py
+++ sm/status/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Status',
+            name='Model',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=45, unique=True)),
