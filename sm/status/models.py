cb078e2d71f0bba2cc87ce0e8af368bc7b4f04dc
Oliver Falk <oliver@linux-kernel.at>
2017-09-07 14:28:36 +0200

    Abstraction for Status

--- sm/status/models.py
+++ sm/status/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from natural_keys import NaturalKeyModel
+from django.urls import reverse
 
 from . import app_label
 
@@ -9,6 +10,9 @@ class Model(NaturalKeyModel):
 
     def __str__(self):
         return "%s" % self.name
+
+    def get_absolute_url(self):
+        return reverse('%s:detail' % app_label, kwargs={'pk': self.pk})
 
     class Meta:
         managed = True
