23dbfa3b6ebe12a08fc1e52014692b1bf7c3f987
Oliver Falk <oliver@linux-kernel.at>
2017-09-08 14:44:55 +0200

    Fix vendor relation + string representation

--- sm/clustersoftware/models.py
+++ sm/clustersoftware/models.py
@@ -9,15 +9,16 @@ from . import app_label
 class Model(NaturalKeyModel):
     name = models.CharField(max_length=45)
     version = models.CharField(max_length=45, default='', blank=True)
-    vendor = models.ForeignKey(VendorModel, null=True, default=None,
+    vendor = models.ForeignKey(VendorModel,
                                related_name='%s_set' % app_label,
                                related_query_name='%s' % app_label)
 
     def __str__(self):
-        if self.version:
-            return "%s %s %s" % (self.vendor.name, self.name, self.version)
-        else:
-            return "%s %s" % (self.vendor.name, self.name)
+        if self.name and self.version:
+            return '%s %s' % (self.name, self.version)
+        if self.name and not self.version:
+            return '%s' % (self.name)
+        return self
 
     def get_absolute_url(self):
         return reverse('%s:detail' % app_label, kwargs={'pk': self.pk})
