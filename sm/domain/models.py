bb923635076b26df66c86b732623597d513eeccb
Oliver Falk <oliver@linux-kernel.at>
2017-12-14 10:39:58 +0100

    Add bool defining if the domain whois should be checked or not and admin/tech_email

--- sm/domain/models.py
+++ sm/domain/models.py
@@ -7,6 +7,9 @@ from . import app_label
 
 class Model(NaturalKeyModel):
     name = models.CharField(max_length=45, unique=True, verbose_name='Domain name')
+    check_whois = models.BooleanField(default=True, verbose_name='Check whois via monitoring?')
+    admin_email = models.CharField(max_length=45, unique=False, verbose_name='Whois admin email to check', blank=True, null=True)
+    tech_email = models.CharField(max_length=45, unique=False, verbose_name='Whois tech email to check', blank=True, null=True)
 
     def __str__(self):
         return "%s" % self.name
