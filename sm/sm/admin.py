d8a99aee483b7c4b4da5e560a4ffa6eb3d6b9419
Oliver Falk <oliver@linux-kernel.at>
2018-01-03 14:42:34 +0100

    Add Cluster to admin

--- sm/sm/admin.py
+++ sm/sm/admin.py
@@ -8,6 +8,7 @@ from domain.models import Model as DomainModel
 from location.models import Model as LocationModel
 from vendor.models import Model as VendorModel
 from operatingsystem.models import Model as OperatingsystemModel
+from cluster.models import Model as ClusterModel
 
 admin.site.register(PatchtimeModel)
 admin.site.register(ServerModel)
@@ -17,3 +18,4 @@ admin.site.register(DomainModel)
 admin.site.register(LocationModel)
 admin.site.register(VendorModel)
 admin.site.register(OperatingsystemModel)
+admin.site.register(ClusterModel)
