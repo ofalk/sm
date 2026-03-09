0ce679d3c72f777c21c9c76b3d817f543843a080
Oliver Falk <oliver@linux-kernel.at>
2026-03-09 10:49:37 +0100

    Fix operatingsystem list view and stabilize tests
    - Restored get_queryset in operatingsystem.ListView to group by vendor.
    - Fixed empty vendor column in operatingsystem list view.
    - Updated operatingsystem.test_views to align with the grouped vendor logic.
    - Ensured all 186 tests pass consistently.

--- sm/operatingsystem/test_views.py
+++ sm/operatingsystem/test_views.py
@@ -2,15 +2,16 @@ from django.contrib import messages
 from django.test import TestCase
 from django.test import Client
 
-from . models import Model
+from .models import Model
 from vendor.models import Model as VendorModel
-from . forms import FormDisabled
-from . forms import Form
+from .forms import FormDisabled
+from .forms import Form
 from . import app_label
 
 from django.contrib.auth.models import User
 
 from django.core.exceptions import ObjectDoesNotExist
+
 try:
     from django.urls import reverse
 except Exception as e:  # pragma: no cover
@@ -27,9 +28,10 @@ class Tester(TestCase):
     teststring = random_string()
     testitem = None
     password = random_string()
-    fixtures = ['%s/fixtures/01_initial.yaml' % 'vendor',
-                '%s/fixtures/01_initial.yaml' % app_label
-                ]
+    fixtures = [
+        "%s/fixtures/01_initial.yaml" % "vendor",
+        "%s/fixtures/01_initial.yaml" % app_label,
+    ]
 
     def login(self):
         """
@@ -48,160 +50,148 @@ class Tester(TestCase):
             password=self.password,
         )
 
-        self.vendor = VendorModel.objects.all().first()
+        self.vendor = VendorModel.objects.all().order_by("name").first()
         self.testitem, created = Model.objects.get_or_create(
             version=self.teststring,
             vendor=self.vendor,
         )
 
     def test_login_redir(self):
-        response = self.client.get(reverse('%s:index' % app_label))
-        self.assertEqual(response.status_code, 302, 'no redirect?')
+        response = self.client.get(reverse("%s:index" % app_label))
+        self.assertEqual(response.status_code, 302, "no redirect?")
 
     def test_listview(self):
         Model.objects.all().delete()
         self.setUp()
         self.login()
-        response = self.client.get(reverse('%s:index' % app_label))
-        self.assertEqual(response.status_code, 200, 'no status 200?')
-        item = response.context[-1]['object_list'].first()
-        self.assertIsInstance(item, Model,
-                              'object not the correct model!?')
-        self.assertEqual(item.version,
-                         self.teststring)
-        self.assertEqual(item.vendor.name,
-                         self.vendor.name)
+        response = self.client.get(reverse("%s:index" % app_label))
+        self.assertEqual(response.status_code, 200, "no status 200?")
+        item = response.context[-1]["object_list"].first()
+        # It should be a VendorModel because of the grouped view
+        self.assertIsInstance(item, VendorModel, "object not the correct model!?")
+        self.assertEqual(
+            item.operatingsystem_set.all().first().version, self.teststring
+        )
 
     def test_detailview(self):
         self.login()
-        url = reverse('%s:detail' % app_label, args=[self.testitem.pk])
-        self.assertEqual('/%s/detail/%i/' % (app_label, self.testitem.pk), url)
+        url = reverse("%s:detail" % app_label, args=[self.testitem.pk])
+        self.assertEqual("/%s/detail/%i/" % (app_label, self.testitem.pk), url)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200, 'no status 200?')
-        item = response.context[-1]['object']
-        self.assertIsInstance(item, Model,
-                              'object not the correct model!?')
+        self.assertEqual(response.status_code, 200, "no status 200?")
+        item = response.context[-1]["object"]
+        self.assertIsInstance(item, Model, "object not the correct model!?")
         self.assertEqual(item.version, self.teststring)
         self.assertEqual(item.vendor.name, self.vendor.name)
-        form = response.context[-1]['form']
+        form = response.context[-1]["form"]
         self.assertIsInstance(form, FormDisabled)
-        for field in ['version', 'vendor']:
-            self.assertTrue(form.fields[field].widget.attrs['disabled'])
+        for field in ["version", "vendor"]:
+            self.assertTrue(form.fields[field].widget.attrs["disabled"])
 
     def test_updateview(self):
         self.login()
-        url = reverse('%s:update' % app_label, args=[self.testitem.pk])
-        self.assertEqual('/%s/update/%i/' % (app_label, self.testitem.pk), url)
+        url = reverse("%s:update" % app_label, args=[self.testitem.pk])
+        self.assertEqual("/%s/update/%i/" % (app_label, self.testitem.pk), url)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200, 'no status 200?')
-        item = response.context[-1]['object']
-        self.assertIsInstance(item, Model,
-                              'object not the correct model!?')
+        self.assertEqual(response.status_code, 200, "no status 200?")
+        item = response.context[-1]["object"]
+        self.assertIsInstance(item, Model, "object not the correct model!?")
         self.assertEqual(item.version, self.teststring)
         self.assertEqual(item.vendor.name, self.vendor.name)
-        form = response.context[-1]['form']
+        form = response.context[-1]["form"]
         self.assertIsInstance(form, Form)
-        for field in ['version', 'vendor']:
-            self.assertRaises(KeyError,
-                              form.fields[field].widget.attrs.__getitem__,
-                              'disabled')
+        for field in ["version", "vendor"]:
+            self.assertRaises(
+                KeyError, form.fields[field].widget.attrs.__getitem__, "disabled"
+            )
 
     def test_deleteview(self):
         self.login()
-        url = reverse('%s:delete' % app_label, args=[self.testitem.pk])
-        self.assertEqual('/%s/delete/%i/' % (app_label, self.testitem.pk), url)
+        url = reverse("%s:delete" % app_label, args=[self.testitem.pk])
+        self.assertEqual("/%s/delete/%i/" % (app_label, self.testitem.pk), url)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200, 'no status 200?')
-        item = response.context[-1]['object']
-        self.assertIsInstance(item, Model,
-                              'object not the correct model!?')
+        self.assertEqual(response.status_code, 200, "no status 200?")
+        item = response.context[-1]["object"]
+        self.assertIsInstance(item, Model, "object not the correct model!?")
         self.assertEqual(item.version, self.teststring)
         self.assertEqual(item.vendor.name, self.vendor.name)
-        self.assertContains(response, 'Are you sure you want to')
-        self.assertContains(response, '<strong>delete</strong>')
+        self.assertContains(response, "Are you sure you want to")
+        self.assertContains(response, "<strong>delete</strong>")
 
     def test_deleteview_post(self):
         self.login()
         response = self.client.post(
-            reverse('%s:delete' % app_label, args=[self.testitem.pk]),
-            follow=True
+            reverse("%s:delete" % app_label, args=[self.testitem.pk]), follow=True
         )
-        self.assertEqual(response.status_code, 200, 'no status 200?')
-        self.assertRedirects(response,
-                             reverse('%s:index' % app_label),
-                             status_code=302)
-        self.assertContains(response,
-                            '%s was deleted successfully' %
-                            self.testitem.version)
+        self.assertEqual(response.status_code, 200, "no status 200?")
+        self.assertRedirects(response, reverse("%s:index" % app_label), status_code=302)
+        self.assertContains(
+            response, "%s was deleted successfully" % self.testitem.version
+        )
         with self.assertRaises(ObjectDoesNotExist):
             Model.objects.get(version=self.testitem.version, vendor=self.vendor)
 
     def test_createview(self):
         self.login()
-        url = reverse('%s:create' % app_label)
-        self.assertEqual('/%s/create' % app_label, url)
+        url = reverse("%s:create" % app_label)
+        self.assertEqual("/%s/create" % app_label, url)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200, 'no status 200?')
-        self.assertRaises(KeyError,
-                          response.context[-1].__getitem__,
-                          'object')
-        self.assertIn('form', response.context[-1])
-        form = response.context[-1]['form']
-        self.assertRaises(KeyError,
-                          form.fields['version'].widget.attrs.__getitem__,
-                          'disabled')
+        self.assertEqual(response.status_code, 200, "no status 200?")
+        self.assertRaises(KeyError, response.context[-1].__getitem__, "object")
+        self.assertIn("form", response.context[-1])
+        form = response.context[-1]["form"]
+        self.assertRaises(
+            KeyError, form.fields["version"].widget.attrs.__getitem__, "disabled"
+        )
 
     def test_createview_post(self):
         # Make sure we have no objects in there
         Model.objects.all().delete()
         self.login()
         data = {
-            'version': self.teststring,
-            'vendor': self.vendor.pk,
+            "version": self.teststring,
+            "vendor": self.vendor.pk,
         }
         response = self.client.post(
-            reverse('%s:create' % app_label),
+            reverse("%s:create" % app_label),
             data,
             follow=True,
         )
-        self.assertEqual(response.status_code, 200, 'no status 200?')
-        self.assertRedirects(response,
-                             reverse('%s:index' % app_label),
-                             status_code=302)
-        item = response.context[-1]['object_list'].first()
-        self.assertEqual(item.version,
-                         data['version'])
-        self.assertEqual(item.vendor.pk,
-                         data['vendor'])
-        self.assertEqual(item.vendor.name,
-                         self.vendor.name)
+        self.assertEqual(response.status_code, 200, "no status 200?")
+        self.assertRedirects(response, reverse("%s:index" % app_label), status_code=302)
+        item = response.context[-1]["object_list"].first()
+        self.assertIsInstance(item, VendorModel)
+        self.assertEqual(
+            item.operatingsystem_set.all().first().version, data["version"]
+        )
+        self.assertEqual(
+            item.operatingsystem_set.all().first().vendor.pk, data["vendor"]
+        )
 
-        self.assertIsInstance(item, Model)
-        self.assertContains(response, '%s was created successfully' % data['version'])
+        self.assertContains(response, "%s was created successfully" % data["version"])
 
     def test_listview_empty_true_wo_obj(self):
         self.login()
         # Set cookie!
-        self.client.cookies['srvmanager-show_empty'] = 'true'
+        self.client.cookies["srvmanager-show_empty"] = "true"
         # Make sure we have no objects in there
         Model.objects.all().delete()
-        response = self.client.get(reverse('%s:index' % app_label))
-        item = response.context[-1]['object_list'].first()
-        self.assertIsNone(item)
+        response = self.client.get(reverse("%s:index" % app_label))
+        item = response.context[-1]["object_list"].first()
+        # Since it returns all Vendors, CentOS (from fixtures) should still be there
+        self.assertIsInstance(item, VendorModel)
 
     def test_listview_empty_false_w_obj(self):
         self.login()
         # Set cookie!
-        self.client.cookies['srvmanager-show_empty'] = 'false'
+        self.client.cookies["srvmanager-show_empty"] = "false"
         # Make sure we have objects in there
         Model.objects.all().delete()
         self.setUp()
-        self.login() # Need to login again since setUp created a NEW user
-        response = self.client.get(reverse('%s:index' % app_label))
-        item = response.context[-1]['object_list'].first()
-        self.assertIsInstance(item, Model,
-                              'object not the correct model!?')
-        self.assertEqual(item.version,
-                         self.teststring)
-        self.assertEqual(item.vendor.name,
-                         self.vendor.name)
+        self.login()  # New user created in setUp
+        response = self.client.get(reverse("%s:index" % app_label))
+        item = response.context[-1]["object_list"].first()
+        self.assertIsInstance(item, VendorModel, "object not the correct model!?")
+        self.assertEqual(
+            item.operatingsystem_set.all().first().version, self.teststring
+        )
--- sm/operatingsystem/views.py
+++ sm/operatingsystem/views.py
@@ -1,9 +1,7 @@
-from __future__ import unicode_literals
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from . models import Model
-from . forms import Form, FormDisabled
+from .models import Model
+from .forms import Form, FormDisabled
 from . import app_label
 
 from django.views.generic import ListView as GenericListView
@@ -23,66 +21,81 @@ from django.contrib import messages
 
 
 class ListView(LoginRequiredMixin, GenericListView):
-    template_name = '%s/list.html' % app_label
+    template_name = "%s/list.html" % app_label
     model = Model
     paginate_by = 20
     paginate_orphans = paginate_by / 4
-    ordering = 'vendor'
+    ordering = "vendor"
+
+    def get_queryset(self):
+        from vendor.models import Model as VendorModel
+
+        if "srvmanager-show_empty" in self.request.COOKIES:
+            if self.request.COOKIES["srvmanager-show_empty"] == "false":
+                return VendorModel.objects.exclude(operatingsystem=None).order_by(
+                    "name"
+                )
+        return VendorModel.objects.all().order_by("name")
 
 
 class DetailView(LoginRequiredMixin, GenericUpdateView):
-    template_name = '%s/detail.html' % app_label
+    template_name = "%s/detail.html" % app_label
     model = Model
     form_class = FormDisabled
 
 
 class UpdateView(SuccessMessageMixin, LoginRequiredMixin, GenericUpdateView):
     success_message = "%(version)s " + _("was updated successfully")
-    template_name = '%s/edit.html' % app_label
+    template_name = "%s/edit.html" % app_label
     model = Model
     form_class = Form
-    success_url = reverse_lazy('%s:index' % app_label)
+    success_url = reverse_lazy("%s:index" % app_label)
 
     def form_valid(self, form):
         self.object = form.save()
         messages.success(self.request, self.success_message % self.object.__dict__)
         from django.http import HttpResponseRedirect
+
         return HttpResponseRedirect(self.get_success_url())
 
 
 class CreateView(SuccessMessageMixin, LoginRequiredMixin, GenericCreateView):
     success_message = "%(version)s " + _("was created successfully")
 
-    template_name = '%s/edit.html' % app_label
-    fields = '__all__'
+    template_name = "%s/edit.html" % app_label
+    fields = "__all__"
     model = Model
-    success_url = reverse_lazy('%s:index' % app_label)
+    success_url = reverse_lazy("%s:index" % app_label)
 
     def get_initial(self):
         from vendor.models import Model as VendorModel
+
         initial = super().get_initial()
-        if 'vendor' in self.kwargs:
-            initial['vendor'] = VendorModel.objects.filter(
-                pk=self.kwargs['vendor']).first()
+        if "vendor" in self.kwargs:
+            initial["vendor"] = VendorModel.objects.filter(
+                pk=self.kwargs["vendor"]
+            ).first()
         return initial
 
     def form_valid(self, form):
         self.object = form.save()
         messages.success(self.request, self.success_message % self.object.__dict__)
         from django.http import HttpResponseRedirect
+
         return HttpResponseRedirect(self.get_success_url())
 
 
 class DeleteView(LoginRequiredMixin, GenericDeleteView):
     success_message = "%(version)s " + _("was deleted successfully")
-    template_name = '%s/delete.html' % app_label
+    template_name = "%s/delete.html" % app_label
     model = Model
-    success_url = reverse_lazy('%s:index' % app_label)
+    success_url = reverse_lazy("%s:index" % app_label)
 
     def form_valid(self, form):
         success_url = self.get_success_url()
-        msg = self.success_message % {'version': getattr(self.object, 'version')}
+        msg = self.success_message % {"version": getattr(self.object, "version")}
         self.object.delete()
         messages.success(self.request, msg)
         from django.http import HttpResponseRedirect
+
         return HttpResponseRedirect(success_url)
